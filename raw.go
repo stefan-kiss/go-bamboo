9df2880db2685123628899ea3d5bd6a7f7a8a6e9
Stefan Kiss <i.stefan.kiss@gmail.com>
2021-08-12 08:33:10 +0300

    feat: raw requests

---
+++ raw.go
@@ -0,0 +1,41 @@
+package bamboo
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// RawService handles communication with the raw request service
+type RawService service
+
+// RawResponse encapsultes a raw response
+type RawResponse struct {
+	Body string
+}
+
+// GetRaw will send a get request
+func (p *RawService) GetRaw(path string) (string, *http.Response, error) {
+
+	request, err := p.client.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return "", nil, err
+	}
+
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := p.client.Do(request, nil)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if !(response.StatusCode == 200) {
+		return "", nil, &simpleError{fmt.Sprintf("Create returned %d", response.StatusCode)}
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", response, &simpleError{fmt.Sprintf("Read body %s", err)}
+	}
+
+	return string(body), response, nil
+}
